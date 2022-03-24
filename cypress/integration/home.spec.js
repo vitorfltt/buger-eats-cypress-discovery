c00213eec0d7503a5f7287fde7ecc8a5426f5035
Vitor Filetto <36848688+vitorfltt@users.noreply.github.com>
2022-03-24 11:52:58 -0300

    ajustando a suite home para os padrões do cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe("home page", () => {
   it("app deve estar online", () => {
-    cy.viewport(1440, 900); //Redimensiona o tamanho da janela do cypress
-    cy.visit("https://buger-eats.vercel.app");
+    cy.visit("/");
     cy.get("#page-home main h1").should(
       "have.text",
       "Seja um parceiro entregador pela Buger Eats"
